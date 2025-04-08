a2d3635d14136c392d2d369a03f692420353062d
MitchDP <71122278+MitchDP@users.noreply.github.com>
2025-04-07 23:25:42 -0700

    Update script.js

--- docs/meme1/script.js
+++ docs/meme1/script.js
@@ -1,7 +1,11 @@
-document.getElementById('overlay').addEventListener('click', function() {
-  var video = document.getElementById('memeVideo');
-  this.style.display = 'none';
-  video.controls = true;
+function startVideo() {
+  const overlay = document.getElementById('overlay');
+  const container = document.querySelector('.container');
+  const video = document.getElementById('memeVideo');
+
+  overlay.style.display = 'none';
+  container.style.display = 'flex';
+
   video.muted = false;
   video.play();
-});
+}
